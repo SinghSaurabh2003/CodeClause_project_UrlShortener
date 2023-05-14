703135d79fb14d4e649f0936665a5d42ac4b3cc8
sourabhchhonkar@gmail.com <saurabhchhonkar@gmail.com>
2023-05-15 01:38:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Name-Saurabh Singh
+The project Url shortner is created by me. Project is assigned by Code Clause 
+in the internship under the domain of python development. Language used 
+to create Url shortner is python.The code is in the repository. 
---
+++ main.py
@@ -0,0 +1,26 @@
+#Importing all modules
+import pyshorteners
+from tkinter import *
+#creating window of perticular size and colour
+window=Tk()
+window.title("URL Shortner")
+window.geometry("400x300")
+window.configure(bg="red")
+#Function to short
+ip=StringVar()
+a=StringVar()
+op_url=StringVar()
+
+def shorturl():
+    a=ip.get()
+    pys=pyshorteners.Shortener().tinyurl.short(a)
+    op_url.set(pys)
+#Heading
+Label(window,text="Enter Your URL",font="impack 17 bold",bg="black",fg="white").pack(fill="x")
+#Input URL BOX
+Entry(window,textvariable=ip,font="10",bd=4,width=40).pack(pady="25")
+#Enter Button
+Button(window,text="Enter",font="impack 17 bold",bg="black",fg="white",command=shorturl).pack()
+Entry(window,textvariable=op_url,font="impack 12 bold",bg="red",bd=0,width=25).pack(pady="25")
+mainloop()
+
